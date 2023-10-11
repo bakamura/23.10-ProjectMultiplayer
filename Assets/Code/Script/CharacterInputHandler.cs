a7fc5d6eb085c0016ccbd1db798dad76603ae55f
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-10-11 10:41:58 -0300

    amend to previous

--- Assets/Code/Script/CharacterInputHandler.cs
+++ Assets/Code/Script/CharacterInputHandler.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 public class CharacterInputHandler : MonoBehaviour
 {
     private Vector2 _movmentDirection;
+    private Vector2 _mousePosition;
+    private CharacterMovmentHandler _movmentHandler;
+    private bool _isJumping;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        _movmentHandler = GetComponent<CharacterMovmentHandler>();
+    }
+
     void Update()
     {
+        _mousePosition.x = Input.GetAxis("Mouse X");
+        _mousePosition.y = Input.GetAxis("Mouse Y") * -1;
+
+
         _movmentDirection.x = Input.GetAxis("Horizontal");
         _movmentDirection.y = Input.GetAxis("Vertical");
+
+        _movmentHandler.SetViewInputVector(_mousePosition);
+        _isJumping = Input.GetButtonDown("Jump");
     }
 
     public NetworkInputData GetInputData()
     {
         NetworkInputData temp = new NetworkInputData();
 
+        temp.RotationYAxis = _mousePosition.x;
+
         temp.MovmentDirection = _movmentDirection;
+
+        temp.IsJumpPressed = _isJumping;
 
         return temp;
     }
--- Assets/Code/Script/CharacterMovmentHandler.cs
+++ Assets/Code/Script/CharacterMovmentHandler.cs
@@ -6,10 +6,22 @@ using Fusion;
 public class CharacterMovmentHandler : NetworkBehaviour
 {
     private NetworkCharacterControllerPrototypeCustom _playerMovment;
+    private Camera _camera;
+    private Vector2 _viewInput;
+    private float _currentCamRotation;
 
     private void Awake()
     {
         _playerMovment = GetComponent<NetworkCharacterControllerPrototypeCustom>();
+        _camera = FindObjectOfType<Camera>();
+    }
+
+    private void Update()
+    {
+        _currentCamRotation += Time.deltaTime * _viewInput.y;
+        _currentCamRotation = Mathf.Clamp(_currentCamRotation, -90f, 90f);
+
+        _camera.transform.localRotation = Quaternion.Euler(0, _currentCamRotation, 0);
     }
 
     public override void FixedUpdateNetwork()
@@ -19,6 +31,11 @@ public class CharacterMovmentHandler : NetworkBehaviour
             Vector3 movDirection = transform.forward * inputData.MovmentDirection.y + transform.right * inputData.MovmentDirection.x;
             _playerMovment.Move(movDirection.normalized);
         }
+    }
+
+    public void SetViewInputVector(Vector2 value)
+    {
+        _viewInput = value;
     }
 
 }
