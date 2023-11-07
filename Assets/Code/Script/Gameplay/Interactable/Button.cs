0f4a009ab70a7e5034522f83304ce91dc9faa1af
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-11-07 10:51:54 -0300

    button + button timer

--- Assets/Code/Script/Gameplay/Interactable/Button.cs
+++ Assets/Code/Script/Gameplay/Interactable/Button.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 using ProjectMultiplayer.ObjectCategory;
 
 namespace ProjectMultiplayer.ObjectCategory
-{
+{    
     public class Button : NetworkBehaviour, IInteractable, IActivable
     {
-
+        [SerializeField] private IActivable[] _activablesList;
+        private bool _hasBeenPressed;
         public void Interact()
         {
-
+            if (!_hasBeenPressed)
+            {
+                for(int i = 0; i < _activablesList.Length; i++)
+                {
+                    _activablesList[i].Activate();
+                }
+                _hasBeenPressed = true;
+            }
         }
 
         public void Activate()
         {
-
+            if (_hasBeenPressed)
+            {
+                for (int i = 0; i < _activablesList.Length; i++)
+                {
+                    _activablesList[i].Deactivate();
+                }
+                _hasBeenPressed = false;
+            }
         }
 
         public void Deactivate()
--- Assets/Code/Script/Gameplay/Interactable/ButtonTimer.cs
+++ Assets/Code/Script/Gameplay/Interactable/ButtonTimer.cs
@@ -8,10 +8,35 @@ namespace ProjectMultiplayer.ObjectCategory
 {
     public class ButtonTimer : NetworkBehaviour, IInteractable
     {
+        [SerializeField] private IActivable[] _activablesList;
+        [SerializeField, Min(0f)] private float _timerDuration;
+        private bool _hasBeenPressed;
+        private float _currentTime;
 
         public void Interact()
         {
+            if (!_hasBeenPressed)
+            {
+                for (int i = 0; i < _activablesList.Length; i++)
+                {
+                    _activablesList[i].Activate();
+                }
+                _hasBeenPressed = true;
+                StartCoroutine(Timer());
+            }
+        }
 
+        private IEnumerator Timer()
+        {
+            while(_currentTime < _timerDuration)
+            {
+                _currentTime += Time.deltaTime;
+                if(_currentTime >= _timerDuration)
+                {
+                    _hasBeenPressed = false;
+                }
+                yield return null;
+            }
         }
 
     }
