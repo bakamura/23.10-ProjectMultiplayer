752c87bd32898656f9926ab922f0d2051393ba48
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-11-10 17:23:37 -0300

    HUD

--- Assets/Code/Script/Menu/Hud.cs
+++ Assets/Code/Script/Menu/Hud.cs
@@ -1,10 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ProjectMultiplayer.Player;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
 namespace ProjectMultiplayer.UI
 {
     public class Hud : Menu
     {
+        [SerializeField] private CanvasGroup _settingsUI;
+        [SerializeField] private CanvasGroup _hudUI;
 
+        private bool _isPaused;
+
+        private void Toggle(InputAction.CallbackContext ctx)
+        {
+            if(ctx.ReadValue<float>() == 1 && _canvasTransitionCoroutine == null)
+            {
+                if (_isPaused) ChangeCurrentCanvas(_hudUI);                
+                else ChangeCurrentCanvas(_settingsUI);                
+                _isPaused = !_isPaused;
+            }
+        }
+
+        private void OnEnable()
+        {
+            InitializeInputPlayer.Instance.PlayerActions.UI.Cancel.performed += Toggle;
+        }
+
+        private void OnDisable()
+        {
+            InitializeInputPlayer.Instance.PlayerActions.UI.Cancel.performed -= Toggle;
+        }
     }
 }
