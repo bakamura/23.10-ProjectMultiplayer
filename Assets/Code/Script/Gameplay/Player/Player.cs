7fda8af1bf85b5a0ab001e717164a36f8bd89dfb
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-11-14 18:20:37 -0300

    fixes to player input

--- Assets/Code/Script/Gameplay/Player/Player.cs
+++ Assets/Code/Script/Gameplay/Player/Player.cs
@@ -6,8 +6,10 @@ using ProjectMultiplayer.Player.Actions;
 using ProjectMultiplayer.Connection;
 using ProjectMultiplayer.ObjectCategory.Size;
 
-namespace ProjectMultiplayer.Player {
-    public class Player : NetworkBehaviour, IPlayerLeft {
+namespace ProjectMultiplayer.Player
+{
+    public class Player : NetworkBehaviour, IPlayerLeft
+    {
 
         //private bool _canAct = true;
 
@@ -34,6 +36,11 @@ namespace ProjectMultiplayer.Player {
         private Size _size;
         private Shield _shieldAbility;
 
+        private bool _alreadyJumped;
+        private bool _alreadyAction1;
+        private bool _alreadyAction2;
+        private bool _alreadyAction3;
+
         private Vector3 _inputV2ToV3 = Vector3.zero;
         //private WaitForSeconds _damagedAnimationWait;
         [Networked] private TickTimer _respawnTimer { get; set; }
@@ -45,25 +52,40 @@ namespace ProjectMultiplayer.Player {
         private Ray _rayCache;
         public Size Size { get { return _size; } }
 
-        public override void Spawned() {
+        //[ContextMenu("Test")]
+        //private void Test()
+        //{
+        //    Debug.Log(_action1.GetType() == typeof(Jump));
+        //}
+
+        public override void Spawned()
+        {
             _nRigidbody = GetComponent<NetworkRigidbody>();
             _camera = Camera.main;
             _screenSize[0] = Screen.width;
             _screenSize[1] = Screen.height;
             _shieldAbility = GetComponent<Shield>();
+            Debug.Log($"Spawned {name}");
         }
 
-        public override void FixedUpdateNetwork() {
-            if (GetInput(out DataPackInput inputData)) {
+        public override void FixedUpdateNetwork()
+        {
+            if (GetInput(out DataPackInput inputData))
+            {
                 _rayCache = _camera.ScreenPointToRay(_screenSize / 2);
                 Movement(inputData.Movement);
-                if (inputData.Jump) _actionJump.DoAction(_rayCache);
-                if (inputData.Action1) _action1.DoAction(_rayCache);
-                if (inputData.Action2) _action2.DoAction(_rayCache);
-                if (inputData.Action3) _action3.DoAction(_rayCache);
+                if (inputData.Jump != _alreadyJumped && inputData.Jump) _actionJump.DoAction(_rayCache);
+                if (inputData.Action1 != _alreadyAction1 && inputData.Action1) _action1.DoAction(_rayCache);
+                if (inputData.Action2 != _alreadyAction2 && inputData.Action2) _action2.DoAction(_rayCache);
+                if (inputData.Action3 != _alreadyAction3 && inputData.Action3) _action3.DoAction(_rayCache);
+                _alreadyJumped = inputData.Jump;
+                _alreadyAction1 = inputData.Action1;
+                _alreadyAction2 = inputData.Action2;
+                _alreadyAction3 = inputData.Action3;
             }
 
-            if (_respawnTimer.Expired(Runner)) {
+            if (_respawnTimer.Expired(Runner))
+            {
                 _respawnTimer = TickTimer.None;
 
                 transform.position = FindObjectOfType<SpawnAnchor>().GetSpawnPosition(_type);
@@ -72,25 +94,29 @@ namespace ProjectMultiplayer.Player {
             }
         }
 
-        private void Movement(Vector2 direction) {
+        private void Movement(Vector2 direction)
+        {
             _inputV2ToV3[0] = direction.x;
             _inputV2ToV3[2] = direction.y;
 
             _nRigidbody.Rigidbody.AddForce(_inputV2ToV3 * _movementSpeed, ForceMode.Acceleration);
         }
 
-        public void TryDamage() {
+        public void TryDamage()
+        {
             if (_shieldAbility != null || !_shieldAbility.IsShielded) Damaged();
             else _shieldAbility.onBlockBullet.Invoke();
         }
 
-        private void Damaged() {
+        private void Damaged()
+        {
             // Damaged Animation
 
             _respawnTimer = TickTimer.CreateFromSeconds(Runner, 3f);
         }
 
-        public void PlayerLeft(PlayerRef player) {
+        public void PlayerLeft(PlayerRef player)
+        {
 
         }
 
