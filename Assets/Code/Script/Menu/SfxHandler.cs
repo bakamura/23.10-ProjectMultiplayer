a5649f2218ad44628a9eb155e51a16122551f183
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-11-22 10:47:20 -0300

    ui sounds

---
+++ Assets/Code/Script/Menu/SfxHandler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class SfxHandler : MonoBehaviour {
+
+    [Header("UI SFX List")]
+
+    [SerializeField] private AudioClip _uiClickSfx;
+    private float _uiClickDuration;
+    private float _uiClickCurrentCooldown = 0;
+
+    [Header("Cache")]
+
+    private AudioSource _as;
+
+    protected void Awake() {
+        _as = GetComponent<AudioSource>();
+
+        _uiClickDuration = _uiClickSfx.length;
+    }
+
+    private void Update() {
+        _uiClickCurrentCooldown -= Time.deltaTime;
+    }
+
+    private void PlaySfx(AudioClip sfx) {
+        _as.PlayOneShot(sfx);
+    }
+
+    public void UiClickSfx() {
+        if (_uiClickCurrentCooldown <= 0 && _uiClickSfx) {
+            _uiClickCurrentCooldown = _uiClickDuration;
+            PlaySfx(_uiClickSfx);
+        }
+    }
+
+}
